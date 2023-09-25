e62b3782569355870313806d4518390b02b525a0
fernaaaa <ferromay.is@gmail.com>
2023-09-25 16:21:59 -0300

    footer Done

--- my-app/app/components/Footer/Footer.jsx
+++ my-app/app/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
+import facebook from "../../../public/facebook.svg";
+import twitter from "../../../public/twitter.svg";
+import youtube from "../../../public/youtube.svg";
+// import instagram from "../../../public/instagram.svg";
+import Image from "next/image";
+
+import styles from "./Footer.module.css";
 export default function Footer() {
   return (
-    <div>
-      <h1>Iam the footer</h1>
+    <div className={styles.containerFooter}>
+      <div className={styles.icons}>
+        <Image src={facebook} />
+        {/* <Image src={instagram} /> */}
+        <Image src={twitter} />
+        <Image src={youtube} />
+      </div>
+      <div className={styles.links}>
+        <p>Condition of Use</p>
+        <p>Privacy & Policy</p>
+        <p>Press Room</p>
+      </div>
+      <p>© 2021 MovieApp by Fernanda Romay</p>
     </div>
   );
 }
