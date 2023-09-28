9d9a8d424b65dc0d09ca25824e26b3151ad93728
fernaaaa <ferromay.is@gmail.com>
2023-09-28 01:36:15 -0300

    change data api

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -6,7 +6,7 @@ async function moviesData() {
     method: "GET",
     url: "https://imdb-top-100-movies.p.rapidapi.com/",
     headers: {
-      "X-RapidAPI-Key": "2c7876ae04msh7f7c9bda7cdf8e0p10edefjsn35ac5a923781",
+      "X-RapidAPI-Key": "9d2ccda05dmsh565a9c16033c475p1ca19bjsn57d7b80e5933",
       "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
     },
   };
